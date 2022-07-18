26a7c38beefe2dfc224af4bf6619734eaccc17e7
EzequielCastilloH <emcastillo2@espe.edu.ec>
2022-07-18 13:00:34 -0500

    Conexión con Firebase añadida

--- LSECT/app/src/main/java/com/jnmolina/lsect/Frases1.java
+++ LSECT/app/src/main/java/com/jnmolina/lsect/Frases1.java
@@ -1,10 +1,12 @@
 package com.jnmolina.lsect;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Parcelable;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -16,13 +18,30 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.database.core.Tag;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.EventListener;
 import java.util.List;
+import java.util.Objects;
 
 public class Frases1 extends AppCompatActivity {
 
     private ListView listPhrases;
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
     ArrayList<ObjectList> listArrayPhrases = new ArrayList<>();
     //private List<String> listArrayPhrases = new ArrayList<>();
     private ArrayAdapter<String> adapter;
@@ -35,38 +54,35 @@ public class Frases1 extends AppCompatActivity {
         listPhrases = findViewById(R.id.listPhrases);
         btnBack = (ImageButton) findViewById(R.id.btnBackPhrases);
 
-        //Enviando títulos de las frases como ObjectList hacia el ArrayList
-        listArrayPhrases.add(new ObjectList(R.drawable.play, "¡Buenos días!"));
-        listArrayPhrases.add(new ObjectList(R.drawable.play, "¡Buenas tardes!"));
-        listArrayPhrases.add(new ObjectList(R.drawable.play, "¡Buenas noches!"));
-        listArrayPhrases.add(new ObjectList(R.drawable.play, "BUHO"));
-        listArrayPhrases.add(new ObjectList(R.drawable.play, "BUHO"));
-        listArrayPhrases.add(new ObjectList(R.drawable.play, "BUHO"));
-        listArrayPhrases.add(new ObjectList(R.drawable.play, "BUHO"));
-        listArrayPhrases.add(new ObjectList(R.drawable.play, "BUHO"));
-        listArrayPhrases.add(new ObjectList(R.drawable.play, "BUHO"));
-        listArrayPhrases.add(new ObjectList(R.drawable.play, "BUHO"));
-        listArrayPhrases.add(new ObjectList(R.drawable.play, "BUHO"));
-        listArrayPhrases.add(new ObjectList(R.drawable.play, "BUHO"));
-        listArrayPhrases.add(new ObjectList(R.drawable.play, "BUHO"));
-        listArrayPhrases.add(new ObjectList(R.drawable.play, "BUHO"));
-        listArrayPhrases.add(new ObjectList(R.drawable.play, "BUHO"));
-        listArrayPhrases.add(new ObjectList(R.drawable.play, "BUHO"));
+        db.collection("frases1").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
 
-
-        //Adaptando el envío de datos
-
-        listPhrases.setAdapter(new AdaptadorLista(this, R.layout.listview,listArrayPhrases){
             @Override
-            public void onEntrada(Object entrada, View view) {
-                TextView texto_superior_entrada = (TextView) view.findViewById(R.id.txtPhraseTitle);
-                ImageView imagen_entrada = (ImageView) view.findViewById(R.id.imgPlay);
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()){
+                    for (QueryDocumentSnapshot document : task.getResult()){
+                        String frase = document.getData().get("frase").toString();
+                        Log.d("Dato",document.getId() + " => " + frase);
 
-                texto_superior_entrada.setText(((ObjectList) entrada).getPhraseTitle());
-                imagen_entrada.setImageResource(((ObjectList) entrada).getIdImage());
+                        listArrayPhrases.add(new ObjectList(R.drawable.play, frase));
+
+                    }
+                } else {
+                    Log.w("Error", "Error", task.getException());
+                }
+                //Adaptando el envío de datos
+
+                listPhrases.setAdapter(new AdaptadorLista(Frases1.this, R.layout.listview,listArrayPhrases){
+                    @Override
+                    public void onEntrada(Object entrada, View view) {
+                        TextView texto_superior_entrada = (TextView) view.findViewById(R.id.txtPhraseTitle);
+                        ImageView imagen_entrada = (ImageView) view.findViewById(R.id.imgPlay);
+
+                        texto_superior_entrada.setText(((ObjectList) entrada).getPhraseTitle());
+                        imagen_entrada.setImageResource(((ObjectList) entrada).getIdImage());
+                    }
+                });
             }
         });
-
 
         listPhrases.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -96,6 +112,4 @@ public class Frases1 extends AppCompatActivity {
 
 
     }
-
-
 }
